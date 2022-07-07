aebbfffc1fb9ad788e051b9fb6f1b1a88fecaef4
Igor Estevao <igor.1999.gigs@gmail.com>
2022-07-07 15:28:06 -0300

    Hello World

---
+++ hello.rs
@@ -0,0 +1,14 @@
+// This is a comment, and is ignored by the compiler
+// You can test this code by clicking the "Run" button over there ->
+// or if you prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
+
+// This code is editable, feel free to hack it!
+// You can always return to the original code by clicking the "Reset" button ->
+
+// This is the main function
+fn main() {
+  // Statements here are executed when the compiled binary is called
+  let name = "Igor, this is the first compiled code of my Rust program";
+  // Print text to the console
+  println!("Hello World! {}", name);
+}
